49a0e4d2b799b2c701db21f933dda5651dd37e64
Zaclose <khan.zafar@gmail.com>
2023-12-11 13:28:36 -0500

    hey bwo witewy

--- app/src/main/java/com/example/tzpizzaapp/CurrentOrderActivity.java
+++ app/src/main/java/com/example/tzpizzaapp/CurrentOrderActivity.java
@@ -18,6 +18,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ the current order screen for the app
+ @author Tyler Amalfa, Zafar Khan
+ */
 public class CurrentOrderActivity extends AppCompatActivity {
     List<PizzaItem> items;
     int selectedPos;
@@ -25,6 +29,11 @@ public class CurrentOrderActivity extends AppCompatActivity {
     RecyclerView pizzaView;
     Order order;
     Button removeItem;
+
+    /**
+     creates the screen for the current order page
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +54,10 @@ public class CurrentOrderActivity extends AppCompatActivity {
         total = findViewById(R.id.total);
         updatePrice();
     }
+
+    /**
+     updates the price of the current order
+     */
     private void updatePrice() {
         String subtotalString = "$" + String.format("%.2f", order.getSubtotal());
         subtotal.setText(subtotalString);
@@ -53,6 +66,11 @@ public class CurrentOrderActivity extends AppCompatActivity {
         String totalString = "$" + String.format("%.2f", order.getTotal());
         total.setText(totalString);
     }
+
+    /**
+     removes a pizza from the order
+     * @param view
+     */
     public void onRemoveButtonClick(View view) {
         order.removePizza(order.getPizzaList().get(selectedPos));
         items = order.getPizzaItemList();
@@ -62,6 +80,11 @@ public class CurrentOrderActivity extends AppCompatActivity {
         removeItem.setEnabled(false);
         updatePrice();
     }
+
+    /**
+     adds an order to the store orders
+     * @param view
+     */
     public void onPlaceButtonClick(View view) {
         if(!order.getPizzaItemList().isEmpty()) {
             StoreOrders.getInstance().newOrder();
@@ -74,6 +97,10 @@ public class CurrentOrderActivity extends AppCompatActivity {
             toast.show();
         }
     }
+
+    /**
+     contains the recycler
+     */
     private class CurrentOrderAdapter extends RecyclerView.Adapter<CurrentOrderAdapter.PizzaItemViewHolder> {
         private Context context;
         public CurrentOrderAdapter(Context context) {
@@ -84,6 +111,13 @@ public class CurrentOrderActivity extends AppCompatActivity {
         public PizzaItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             return new PizzaItemViewHolder(LayoutInflater.from(context).inflate(R.layout.pizza_item_view, parent,  false));
         }
+
+        /**
+         generates new item holders as you scroll down the view
+         * @param holder The ViewHolder which should be updated to represent the contents of the
+         *        item at the given position in the data set.
+         * @param position The position of the item within the adapter's data set.
+         */
         @Override
         public void onBindViewHolder(@NonNull PizzaItemViewHolder holder, int position) {
             Pizza p = items.get(position).getPizza();
@@ -112,6 +146,10 @@ public class CurrentOrderActivity extends AppCompatActivity {
             holder.name.setText(items.get(position).getName());
             holder.itemView.setBackgroundColor(selectedPos == position ? Color.LTGRAY : Color.TRANSPARENT);
         }
+
+        /**
+         holds each individual item
+         */
         public class PizzaItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
             ImageView pizzaImage;
             TextView name, sauce, toppingsList, size, price;
@@ -125,6 +163,11 @@ public class CurrentOrderActivity extends AppCompatActivity {
                 price = itemView.findViewById(R.id.price);
                 itemView.setOnClickListener(this);
             }
+
+            /**
+             selects the PizzaItem holder
+             * @param view
+             */
             @Override
             public void onClick(View view) {
                 if (getAdapterPosition() == RecyclerView.NO_POSITION) removeItem.setEnabled(false);
@@ -136,6 +179,11 @@ public class CurrentOrderActivity extends AppCompatActivity {
                 }
             }
         }
+
+        /**
+         returns the number of items in the order as an int
+         @return number of items
+         */
         @Override
         public int getItemCount() {
             return items.size();
--- app/src/main/java/com/example/tzpizzaapp/MainMenuActivity.java
+++ app/src/main/java/com/example/tzpizzaapp/MainMenuActivity.java
@@ -6,24 +6,52 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ the main menu screen for the app
+ @author Tyler Amalfa, Zafar Khan
+ */
 public class MainMenuActivity extends AppCompatActivity {
+    /**
+     creates the screen for the main menu
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
     }
+
+    /**
+     opens the build your own activity screen
+     @param view
+     */
     public void buildClick(View view) {
         Intent intent = new Intent(this, BuildYourOwnActivity.class);
         startActivity(intent);
     }
+
+    /**
+     opens the specialty activity screen
+     * @param view
+     */
     public void specialtyClick(View view) {
         Intent intent = new Intent(this, SpecialtyActivity.class);
         startActivity(intent);
     }
+
+    /**
+     opens the current order activity screen
+     * @param view
+     */
     public void currentOrderClick(View view) {
         Intent intent = new Intent(this, CurrentOrderActivity.class);
         startActivity(intent);
     }
+
+    /**
+     opens the store orders activity screen
+     * @param view
+     */
     public void storeOrdersClick(View view) {
         if(StoreOrders.getInstance().getOrders().size() > 1) {
             Intent intent = new Intent(this, StoreOrdersActivity.class);
